a6e5ef2a70565020a7652f928bf4ef533a489e85
Doctor Gorgoner <DoctorGorgoner@gmail.com>
2018-07-25 14:36:11 +0300

    Тестовое Задание для KupiVip

---
+++ assets/database/productsDb.js
@@ -0,0 +1,201 @@
+const products = [
+  {
+    name: 'Босоножки',
+    article: 'GARSIA1-6-11/20',
+    images: [
+      'teal.jpg',
+      'white.jpg'
+    ],
+    oldPrice: 5990,
+    price: 2995,
+    labels: {
+      new: true,
+      sale: 50
+    },
+    colors: [
+      'teal',
+      'white'
+    ],
+    sizes: [
+      {
+        value: 35,
+        available: false
+      }, {
+        value: 36,
+        available: true
+      }, {
+        value: 37,
+        available: true
+      }, {
+        value: 38,
+        available: true
+      }, {
+        value: 39,
+        available: true
+      }, {
+        value: 40,
+        available: true
+      }
+    ]
+  }, {
+    name: 'Босоножки',
+    article: 'VIRGIL2-1-5',
+    images: [
+      'black.jpg',
+      'brown.jpg'
+    ],
+    oldPrice: 6990,
+    price: 3495,
+    labels: {
+      new: true,
+      sale: 50
+    },
+    colors: [
+      'black',
+      'brown'
+    ],
+    sizes: [
+      {
+        value: 36,
+        available: false
+      }, {
+        value: 37,
+        available: true
+      }, {
+        value: 38,
+        available: true
+      }, {
+        value: 39,
+        available: true
+      }, {
+        value: 40,
+        available: true
+      }
+    ]
+  }, {
+    name: 'Кроссовки',
+    article: 'HELGA1-5-45',
+    images: [
+      'khaki.jpg'
+    ],
+    oldPrice: 6990,
+    price: 3495,
+    labels: {
+      new: true,
+      sale: 50
+    },
+    colors: [
+      'khaki'
+    ],
+    sizes: [
+      {
+        value: 36,
+        available: true
+      }, {
+        value: 37,
+        available: true
+      }, {
+        value: 38,
+        available: true
+      }, {
+        value: 39,
+        available: true
+      }, {
+        value: 40,
+        available: false
+      }
+    ]
+  }, {
+    name: 'Рюкзак',
+    article: 'H6908-2',
+    images: [
+      'beije.jpg',
+      'khaki.jpg'
+    ],
+    oldPrice: false,
+    price: 8990,
+    labels: {
+      new: false,
+      sale: 0
+    },
+    colors: [
+      'beije',
+      'khaki'
+    ],
+    sizes: [
+      {
+        value: 'NS',
+        available: true
+      }
+    ]
+  }, {
+    name: 'Сабо',
+    article: 'RINA2-3-5',
+    images: [
+      'red.jpg'
+    ],
+    oldPrice: 5490,
+    price: 3843,
+    labels: {
+      new: true,
+      sale: 30
+    },
+    colors: [
+      'red'
+    ],
+    sizes: [
+      {
+        value: 36,
+        available: true
+      }, {
+        value: 37,
+        available: true
+      }, {
+        value: 38,
+        available: true
+      }, {
+        value: 39,
+        available: true
+      }, {
+        value: 40,
+        available: true
+      }, {
+        value: 41,
+        available: true
+      }
+    ]
+  }, {
+    name: 'Туфли',
+    article: 'DOLY1-6-12',
+    images: [
+      'blue.jpg'
+    ],
+    oldPrice: 7490,
+    price: 2247,
+    labels: {
+      new: true,
+      sale: 70
+    },
+    colors: [
+      'blue'
+    ],
+    sizes: [
+      {
+        value: 36,
+        available: true
+      }, {
+        value: 37,
+        available: false
+      }, {
+        value: 38,
+        available: true
+      }, {
+        value: 39,
+        available: true
+      }, {
+        value: 40,
+        available: false
+      }
+    ]
+  }
+]
---
+++ script.js
@@ -0,0 +1,147 @@
+function createCard(product, productIndex, container, sorted) {
+  let labels = '',
+      card,
+      imgSrc,
+      oldPrice = '',
+      price,
+      sizes,
+      colors;
+
+  imgSrc = 'assets/pictures/product' + productIndex + '/' + product.images[0];
+
+  if (product.labels.new) labels = '<div class="label">NEW</div>';
+
+  if (product.labels.sale) {
+    labels += '<div class="label discount">- ' + product.labels.sale + '%</div>'
+  };
+
+  if (product.oldPrice) {
+    oldPrice =
+      '<div class="price old-price">' +
+        number_format(product.oldPrice, 0, '', ' ') +
+      '</div>'
+  };
+
+  price =
+    '<div class="price ' +
+      (oldPrice == '' ? '">' : 'new-price">') +
+      number_format(product.price, 0, '', ' ') +
+    '</div>';
+
+  sizes = '<span class="sizes-title uppercase">Размеры: </span>';
+
+  for (let i = 0; i < product.sizes.length; i++) {
+    let unavailable = product.sizes[i].available == false ? 'unavailable' : '';
+
+    sizes +=
+      '<span class="size ' + unavailable + '">' +
+        product.sizes[i].value +
+      ' </span>';
+  };
+
+  colors = '<span class="colors-title uppercase">Цвета: </span>';
+
+  for (let i = 0; i < product.colors.length; i++) {
+    colors +=
+      '<div class="color' + (i == 0 ? ' active' : '') + '" ' +
+      'data-color="' + product.colors[i] + '"' + '>' +
+        '<div class="' + product.colors[i] + '"></div>' +
+      '</div>';
+  };
+
+  card =
+    '<div class="product-card ' + (sorted ? 'expanded' : '') +
+    '" data-index="' + productIndex + '"><a>' +
+      '<div class="labels">' + labels + '</div>' +
+      '<div class="img">' +
+        '<img src="' + imgSrc + '">' +
+      '</div>' +
+      '<div class="main-info">' +
+        '<div class="name uppercase">' +
+          product.name +
+        '</div>' +
+        '<div class="article">Арт. ' +
+          product.article +
+        '</div>' +
+        '<div class="price-block">' +
+          oldPrice +
+          price +
+        '</div>' +
+      '</div>' +
+      '<div class="other-info">' +
+        '<div class="sizes">' +
+          sizes +
+        '</div>' +
+        '<div class="colors">' +
+          colors +
+        '</div>' +
+      '</div>' +
+    '</a></div>';
+
+  container.append(card);
+};
+
+function renderCards(container, filterStore) {
+  if (filterStore) {
+    let sortable = [];
+
+    for (let i = 0; i < products.length; i++) {
+      sortable.push([i, products[i].price]);
+    };
+
+    sortable.sort(function(a, b) {
+      if (a[1] > b[1]) return -1;
+      if (a[1] < b[1]) return 1;
+    });
+
+    sortable = sortable.map(function(item) {
+      return item[0];
+    }).slice(0, 3);
+
+    for (let i = 0; i < products.length; i++) {
+      if (sortable.indexOf(i) != -1) {
+        createCard(products[i], i, container, filterStore);
+      }
+    };
+  } else {
+    for (let i = 0; i < products.length; i++) {
+      createCard(products[i], i, container);
+    };
+  }
+
+  $('.color').on('click', function() {
+    let colorButton = $(this),
+        color = colorButton.attr('data-color'),
+        productCard,
+        productIndex,
+        picture;
+
+    colorButton.addClass('active').siblings().removeClass('active');
+
+    productCard = colorButton.closest('.product-card');
+
+    productIndex = productCard.attr('data-index');
+
+    picture = productCard.find('img');
+
+    picture.attr(
+      'src',
+      'assets/pictures/product' + productIndex + '/' + color + '.jpg'
+    );
+  });
+};
+
+$(function() {
+  const showcase = $('.showcase'),
+        filterCheckbox = $('input');
+
+  renderCards(showcase, filterCheckbox.is(':checked'));
+
+  filterCheckbox.change(function() {
+    let filterStore = filterCheckbox.is(':checked');
+
+    showcase.empty();
+
+    renderCards(showcase, filterStore);
+  });
+});
